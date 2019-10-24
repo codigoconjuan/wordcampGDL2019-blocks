d52a721a14d2ec986b64a7ede19e0a0f0d7eb503
Juan Pablo De la torre Valdez <juanpablodelatorrevaldez@la-MacBook-Pro-de-Juan.local>
2019-10-24 14:52:05 -0500

    primer commit

---
+++ src/bloque/index.js
@@ -0,0 +1,11 @@
+/*
+    Pasos para crear Bloques
+    1.- Crear la estructura básica con registerBlockType
+    2.- Importar el componente(s) que utilizarás
+    3.- Coloca el componente donde deseas utilizarlo.
+    4.- Crea una función que lea los contenidos
+    5.- Registra un atributo
+    6.- Extraer el contenido desde props
+    7.- Guarda el contenido con setAttributes
+    8.- Lee los contenidos guardados en save()
+ */
